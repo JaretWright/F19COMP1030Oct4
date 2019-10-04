2891555d406e45800179e2fca6e8b8ce29d4dde4
Jaret Wright <hkkA223>
2019-10-04 10:49:57 -0400

    Added Dice display in the main method

--- src/PlayingWithDice.java
+++ src/PlayingWithDice.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PlayingWithDice {
     public static void main(String[] args)
     {
@@ -5,5 +7,29 @@ public class PlayingWithDice {
         Dice die2 = new Dice(6);
 
         System.out.printf("die1: %s     die2: %s %n", die1, die2);
+        die1.rollDice();
+        System.out.printf("die1: %s     die2: %s %n", die1, die2);
+        die2.rollDice();
+        System.out.printf("die1: %s     die2: %s %n", die1, die2);
+
+        int numOfSides=0;
+        Dice die3;
+
+        while (numOfSides==0)
+        {
+            try {
+                System.out.printf("Enter how many sides you want on the Dice: ");
+                Scanner keyboard = new Scanner(System.in);
+                numOfSides = keyboard.nextInt();
+                die3 = new Dice(numOfSides);
+                System.out.printf("die1: %s     die2: %s    die3: %s%n"
+                        , die1, die2, die3);
+            } catch (IllegalArgumentException e) {
+                System.out.printf("%s%n", e.getMessage());
+                numOfSides=0;
+            }
+        }
+
+
     }
 }
